40bbc5655c99e2c53c0dd10311251f5fbeb65180
Navya <navyaprasad127@gmail.com>
2025-09-17 10:10:57 +0530

    Day 1:python basics

---
+++ day1.py
@@ -0,0 +1,22 @@
+print("hello, welcome to python learning")
+age= 23
+name = "navya"
+pi = 3.14159
+is_student = True
+
+print("Name",name)
+print("Age", age)
+print("value of pi", pi)
+print("Are you a student", is_student)
+
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_student))
+
+num_str ="100"
+num_int = int(num_str)
+print("after conversion:", num_int +50)
+
+float_num = float(age)
+print("age as float:", float_num)
